bcdf8e3149139f532311e5e2097b8e289970f38a
sneakyfildy <feeldeeng@gmail.com>
2016-03-14 12:58:09 +0400

    RT.getTi cket impl

---
+++ rti.js
@@ -0,0 +1,145 @@
+/* global module, define */
+(function () {
+    function moduleDefiniton() {
+        /**
+         * Manage RT with its REST API
+         * @class RT
+         * @param {Object} [cfg]
+         * @param {String} [cfg.url]
+         * @singleton
+         */
+        function RT(cfg) {
+
+            function init() {
+                this.url = cfg.url || this.url; // always put a property in instance, I wanna see it in console
+            }
+
+            /**
+             *
+             * @param {Number|String} id
+             * @param {Function} [callback]
+             * callback param:
+             *      {
+             *          success: true,
+             *          params: {
+             *              id: "1",
+             *              owner: "you"
+             *          },
+             *          errorText: ""
+             *      }
+             * @return {undefined}
+             */
+            this.getTicket = function (id, callback) {
+                var fullUrl;
+                fullUrl = this.url + this.getTicketPattern.replace('%id%', id);
+                this.ajax({
+                    method: 'POST',
+                    url: fullUrl,
+                    success: this.onGetTicket.bind(this, callback),
+                    error: this.onGetTicketError.bind(this, callback)
+                });
+            };
+
+            return init.call(this);
+        }
+
+
+        var protoStuff = {
+            /**
+             * @param {String}
+             */
+            url: 'http://rt.easter-eggs.org/demos/4.2/',
+            getTicketPattern: 'ticket/%id%/show',
+            /**
+             * Custom AJAX function, implement it yourself if wanna be jquery-independent
+             */
+            nonJqueryAjax: null,
+            onGetTicket: function (callback, res) {
+                var parsedRes = this.parseResponse(res);
+
+                if (!parsedRes.success){
+                    console.error('Error getting ticket data: ', parsedRes.errorText || '<No error text>');
+                }else if ( callback && typeof callback === 'function' ){
+                    callback(parsedRes); // assuming scope and everything is bound already
+                }
+            },
+            onGetTicketError: function (callback, res) {debugger;
+                var errorText = res.statusText || '<No error text>';
+                console.error('Error getting ticket data: ', errorText, ' Status: ' + res.status);
+
+                if ( callback && typeof callback === 'function' ){
+                    callback({
+                        success: false,
+                        params: {},
+                        errorText: errorText
+                    });
+                }
+            },
+            parseResponse: function(ticketResponse){
+                var r, statusLine, emptyLine, possibleErrorLine, params, line, success, errorText;
+                params = {};
+                success = true;
+                errorText ='';
+                r = ticketResponse;
+                r = r.split('\n');
+
+                statusLine = r.shift();
+                emptyLine = r.shift();
+                possibleErrorLine = r[0];
+
+                if (!statusLine || statusLine.toLowerCase().indexOf('200 ok') < 0 || emptyLine !== ''){
+                    errorText = 'Something went wrong, incorrect server answer structure.';
+                    success = false;
+                }else if ( possibleErrorLine && possibleErrorLine.indexOf('#') === 0 ){
+                    possibleErrorLine = possibleErrorLine.replace('#', '').replace(/^\s+/gim, '');
+                    errorText = possibleErrorLine;
+                    success = false;
+                }else{
+                    for (var i = 0; i < r.length; i++){
+                        line = r[i];
+                        if (line !== '' && !!line && line.indexOf(': ')){
+                            line = line.split(': ');
+                            if ( line.length === 2 ){
+                                if (line[0] === 'id'){
+                                    line[1] = line[1].replace('ticket/', '');
+                                }
+                                params[line[0]] = line[1];
+                            }
+                        }
+                    }
+                }
+
+                return {
+                    success: success,
+                    errorText: errorText,
+                    params: params
+                };
+            },
+            ajax: function(ajaxParams){
+                if (this.nonJqueryAjax){
+                    return this.nonJqueryAjax(ajaxParams);
+                }else{
+                    return $.ajax(ajaxParams);
+                }
+            }
+        };
+
+        for (var stuffName in protoStuff) {
+            if (typeof protoStuff[stuffName] === 'function') {
+                RT.prototype[stuffName] = protoStuff[stuffName].bind(RT.prototype);
+            } else {
+                RT.prototype[stuffName] = protoStuff[stuffName];
+            }
+        }
+
+        return RT;
+    }
+
+    if (typeof define === "function" && define.amd) {
+        define([], moduleDefiniton);
+    } else if (typeof module === "object" && module.exports) {
+        module.exports = moduleDefiniton;
+    } else {
+        window['RTIConstructor'] = moduleDefiniton();
+    }
+})();
